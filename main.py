bb3143b144a44a0824b20b71041ba47ed817a51e
Yoshimaru Tanaka <robograffitti@gmail.com>
2024-07-17 16:23:43 +0900

    modified.

--- main.py
+++ main.py
@@ -140,9 +140,9 @@ class VQADataset(torch.utils.data.Dataset):
         # q = process_text(self.df["question"][idx])
         # question_words = q.split()
 
-        # tokenize question
-        print(process_text(self.df["question"][idx]))
+        # tokenize question 
         question = self.tokenizer(process_text(self.df["question"][idx]), max_length=512, padding="max_length", truncation=True, return_tensors='pt')
+        question
         # question = self.tokenizer.encode(process_text(self.df["question"][idx]))
 
         # question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
