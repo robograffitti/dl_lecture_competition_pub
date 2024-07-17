17567adea8a42e76330026de0a6b584accbb8ca0
Yoshimaru Tanaka <robograffitti@gmail.com>
2024-07-17 12:01:42 +0900

    fixed.

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ class VQADataset(torch.utils.data.Dataset):
         image = self.transform(image)
 
         # 質問文の前処理（工夫の例から追加）
-        question = [self.question2idx[process_text(question)] for question in self.df["question"][idx]]
+        question = [self.question2idx[word] for word in process_text(question).split() for question in self.df["question"][idx]]
         # question = self.question2idx[process_text(self.df["question"][idx])]
 
         question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
