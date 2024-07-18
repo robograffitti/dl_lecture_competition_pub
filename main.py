95bd1f2d019e0841328c18e8436616ddb1fe8e98
Yoshimaru Tanaka <robograffitti@gmail.com>
2024-07-18 13:01:00 +0900

    added rand rot.

--- main.py
+++ main.py
@@ -416,7 +416,7 @@ def main():
     # test_ds_rand_rot = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=tf_rand_rot, answer=False)
     
     train_dataset = train_ds_resize + train_ds_rand_rot 
-    test_dataset = test_ds_resize, test_ds_rand_rot
+    test_dataset = test_ds_resize + test_ds_rand_rot
     # test_dataset = torch.utils.data.ConcatDataset([test_ds_resize, test_ds_rand_rot]) 
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
