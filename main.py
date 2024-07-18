4c13956258edc5cbab286554e04a07faaf6f0df1
Yoshimaru Tanaka <robograffitti@gmail.com>
2024-07-18 10:58:26 +0900

    1 epoc.

--- main.py
+++ main.py
@@ -409,7 +409,7 @@ def main():
     model = VQAModel(n_answer=len(train_dataset.answer2idx)).to(device)
 
     # optimizer / criterion
-    num_epoch = 20
+    num_epoch = 1
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
